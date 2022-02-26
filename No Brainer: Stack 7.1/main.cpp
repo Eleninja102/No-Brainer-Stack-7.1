7904a7b9c4c604db9b34ff470d0d53a6b2f8a60c
Coleton Watt <electricninja102@gmail.com>
2022-02-26 14:58:51 -0700

    Initial Commit

---
+++ No Brainer: Stack 7.1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  No Brainer: Stack 7.1
+//
+//  Created by Coleton Watt on 2/26/22.
+//
+/*
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+*/
